396e4a81f103d019471cf45e94d00959685abfbf
longlonglu <48895042+longlonglu@users.noreply.github.com>
2021-08-26 22:33:30 -0400

    added nodemailer, move playwirght to a separete folder

---
+++ timer_task/daily_report/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  CLIENT_ID:
+    "630647017025-7dtpmh018j7s5qiuvrp79q9fg8lh3rai.apps.googleusercontent.com",
+  CLIENT_SECRET: "KL2RRRhYjqzD-j9761fjymsz",
+  REDIRECT_URI: "https://developers.google.com/oauthplayground",
+  REFRESH_TOKEN:
+    "1//04xRUOLrpHeHnCgYIARAAGAQSNwF-L9IrxyuL7mgvssy-feIQTJjBofQUDj56QM6N6FzlFGSqDzayDhDp9CoGO1RX5R1L14gJ0aw",
+  USER: "longlonglu68@gmail.com",
+  SUBJECT: "leetcool daily report",
+};
