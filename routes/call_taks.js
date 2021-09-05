c2e59c7812ba1d0c137eab36262dd7e98393de03
longlonglu <48895042+longlonglu@users.noreply.github.com>
2021-09-05 16:49:02 -0400

    fixed bug

--- routes/call_taks.js
+++ routes/call_taks.js
@@ -10,30 +10,35 @@ const { sendMail } = require("../timer_task/daily_report/send_email");
 const DailyReportTemplate = require("../timer_task/daily_report/daily_report_template");
 
 router.get("/send-daily-report", async (req, res) => {
-  const dailyUsers = await DailyQuestion.findOne()
-    .sort({ _id: -1 })
-    .populate({ path: "users", select: ["user_email", "user_name"] })
-    .select("users");
-  const allUsers = await User.find().select("user_email user_name");
-  let recipients = "";
-  allUsers.map((user) => {
-    recipients += user.user_email + ", ";
-  });
-  const subject = "~ o(*￣▽￣*)o Daily Report From Leetcool";
-  const usersDid = dailyUsers.users;
-  const usersDidNot = allUsers.filter(
-    ({ user_email: email1 }) =>
-      !usersDid.some(({ user_email: email2 }) => {
-        return email1 == email2;
-      })
-  );
-  const template = DailyReportTemplate.dailyReportTemplate(
-    usersDid,
-    usersDidNot
-  );
+  try {
+    const dailyUsers = await DailyQuestion.findOne()
+      .sort({ _id: -1 })
+      .populate({ path: "users", select: ["user_email", "user_name"] })
+      .select("users");
+    const allUsers = await User.find().select("user_email user_name");
+    let recipients = "";
+    allUsers.map((user) => {
+      recipients += user.user_email + ", ";
+    });
+    const subject = "~ o(*￣▽￣*)o Daily Report From Leetcool";
+    const usersDid = dailyUsers.users;
+    const usersDidNot = allUsers.filter(
+      ({ user_email: email1 }) =>
+        !usersDid.some(({ user_email: email2 }) => {
+          return email1 == email2;
+        })
+    );
+    const template = DailyReportTemplate.dailyReportTemplate(
+      usersDid,
+      usersDidNot
+    );
 
-  const result = await sendMail(recipients, subject, template);
-  res.json({ template: template, subject: subject, recipients: recipients });
+    const result = await sendMail(recipients, subject, template);
+    res.json({ template: template, subject: subject, recipients: recipients });
+  } catch (error) {
+    console.log("error");
+    res.json("error");
+  }
 });
 
 router.get("/fetch-daily", async (req, res) => {
